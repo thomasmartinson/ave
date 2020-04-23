80603bc5ebcff8d97b6e049313acc2a9167a9687
Tommy Martinson <tmm2@princeton.edu>
2020-04-23 12:09:29 -0400

    refactor ave.py

--- ave.py
+++ ave.py
@@ -15,7 +15,7 @@ shifts = [
     },
     {
         'volunteer': 'Bob B.',
-        'date': 'April 23, 2020',
+        'date': 'March 23, 2020',
         'role': 'lector'
     },
     {
@@ -25,38 +25,47 @@ shifts = [
     }
 ]
 
+# takes in a datetime object
+# returns the volunteers for that month
+def get_volunteers_for(time):
+    return [i for i in shifts if i['date'].split()[0] == time.strftime('%B')]
+
+
+# takes in a datetime object
+# returns a dict for that month in ideal format to display on homepage
+def make_month_dict(time):
+    month = { 
+        'title': '{} {}'.format(time.strftime('%B'), time.year),
+        'weeks': [],
+        'last_day': calendar.monthrange(time.year, time.month)[1], # day of month
+        'volunteers': get_volunteers_for(time)
+    }
+
+    first_day = time.replace(day=1).weekday() # day of week
+    
+    # insert the week lists
+    for wk in range(7):
+        month['weeks'].append([i - first_day + 7 * wk for i in range(7)])
+        if month['weeks'][-1][-1] >= month['last_day']:
+            break # avoid empty weeks
+
+    return month
+
 
 # rendering the home page
 @app.route('/')
 def home():
-    # returns a dict for the month in ideal format to display on homepage
-    # the returned month corresponds to the given datetime object
-    def make_month_dict(time):
-        month = { 
-            'title': '{} {}'.format(time.strftime('%B'), time.year),
-            'weeks': [],
-            'last_day': calendar.monthrange(time.year, time.month)[1] # day of month
-        }
+    dt_now = datetime.now()
+    dt_next_month = dt_now.replace(day=1).replace(month=dt_now.month % 12 + 1)
 
-        first_day = time.replace(day=1).weekday() # day of week
-        
-        # insert the week lists
-        for wk in range(7):
-            month['weeks'].append([i - first_day + 7 * wk for i in range(7)])
-            if month['weeks'][-1][-1] >= month['last_day']:
-                break # avoid empty weeks
-
-        return month
-
-    now = datetime.now()
-    month = make_month_dict(now)
+    this_month = make_month_dict(dt_now)
+    next_month = {}
 
     # display next month if less than a certain number of days away
-    next_month = {}
-    if (month['last_day'] - HEADS_UP_DAYS <= now.day):
-        next_month = make_month_dict(now.replace(day=1).replace(month=now.month % 12 + 1))
+    if (this_month['last_day'] - HEADS_UP_DAYS <= dt_now.day):
+        next_month = make_month_dict(dt_next_month)
 
-    return render_template('home.html', shifts=shifts, month=month, next_month=next_month)
+    return render_template('home.html', month=this_month, next_month=next_month)
 
 
 # extra test page
